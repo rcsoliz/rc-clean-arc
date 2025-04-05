897a21ce7cf74a9b42a06a3b498226b8b298d13a
rcsoliz <robertoucbnh@gmail.com>
2025-04-04 22:22:29 -0400

    resolucion de Comments

--- Core/Entities/Comment.cs
+++ Core/Entities/Comment.cs
@@ -12,6 +12,9 @@ namespace Core.Entities
         public int UserId { get; set; }
         public int PostId { get; set; }
 
+
+
+
         public User User { get; set; }
         public Post Post { get; set; }
 
