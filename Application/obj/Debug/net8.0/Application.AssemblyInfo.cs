50798a51443446485a4e3a842eaaa1618121d6ee
rcsoliz <robertoucbnh@gmail.com>
2025-04-04 23:00:54 -0400

    Cambios en develop

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7814bb7596728c539fd0282315729cfba26cbbe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db900205ff19fa7bd0884d39c042a1ada822db92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
