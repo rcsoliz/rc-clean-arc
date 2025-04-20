12f6930963b15cf9b273aba123864cdf81847c13
rcsoliz <robertoucbnh@gmail.com>
2025-04-20 10:51:21 -0400

    Se agrego el fil site1

---
+++ BlazorAppRc/wwwroot/js/site1.js
@@ -0,0 +1,12 @@
+﻿window.getSelectedOptions = () => {
+    const element = document.getElementById('selectCategorias');
+    const result = [];
+
+    if (!element) return result;
+
+    for (let option of element.selectedOptions) {
+        result.push(option.value);
+    }
+
+    return result;
+};
