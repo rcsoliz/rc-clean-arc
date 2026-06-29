e7c619773f20d33e75eebdda60f5d48578c6c9d5
rcsoliz <robertoucbnh@gmail.com>
2026-06-28 21:00:01 -0400

    add changes

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a2e4a046d921aa7ed6721cbbaba7977a31a8ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e1dba21e66036e97de9d5068b622427f71cf14d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
